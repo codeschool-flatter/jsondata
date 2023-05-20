f333ac58b5b084ee9ae914eabbe6ef55b3828afd
ZARIFMAC <uzbzarif@gmail.com>
2023-05-20 16:26:55 +0500

    get max age

--- bin/jsondata.dart
+++ bin/jsondata.dart
@@ -1,7 +1,21 @@
 import 'dart:convert';
 import '../lib/from_json.dart';
 
+// Find older users name from users.json
+String findOlderUser(List users) {
+  int maxAge = users[0]['age'];
+  String name = '';
+  for (var user in users) {
+    if (user['age'] > maxAge) {
+      maxAge = user['age'];
+      name = "${user['name']['first']} ${user['name']['last']}";
+    }
+  }
+  return name;
+}
+
 void main() {
-  Map user = readJson('user.json');
-  print(user['gender']);
+  Map data = readJson('users.json');
+  List users = data['users'];
+  print(findOlderUser(users));
 }
